b927c483c061ee0263e1367110da9530af6e5d27
Brandon Howe <superspeedy1324@hotmail.com>
2018-01-21 16:38:09 -0500

    Created moving animation for snowman

--- Washed Up Wizard/Assets/Scripts/UI/CombinationsMenu.cs
+++ Washed Up Wizard/Assets/Scripts/UI/CombinationsMenu.cs
@@ -7,10 +7,13 @@ public class CombinationsMenu : MonoBehaviour {
 	public PlayerSpellsReference playerSpellReference;
 	public float highestCombo = 120;
 	public float spaceBetweenCombos = 60;
-	public float combosPerPage = 6;
+	public float combosPerPage = 2;
 
 	private int spellNum = 0;
-	private Transform spellCombo;
+	private int currentPage = 0;
+	private int maxPage;
+	private Transform tempCombo;
+	private List<Transform> combos = new List<Transform> ();
 
 	// Use this for initialization
 	void Start () {
@@ -24,17 +27,51 @@ public class CombinationsMenu : MonoBehaviour {
 
 	void SetUpCombinations () {
 		spellNum = 0;
+		currentPage = 0;
+		combos.Clear ();
 		for (int i = 0; i < playerSpellReference.spells.Length; i++) {
 			for (int j = 0; j < playerSpellReference.spells [i].componentSpells.Length; j++) {
 				if (playerSpellReference.spells [i].componentSpells [j] != null) {
-					spellCombo = transform.Find (playerSpellReference.spells [i].componentSpells [j].name);
-					print (spellCombo.gameObject.name);
-					spellCombo.localPosition = new Vector3 (600, highestCombo - spaceBetweenCombos * spellNum, 0);
+					if (spellNum - combosPerPage * currentPage >= combosPerPage) {
+						currentPage++;
+					}
+					tempCombo = transform.Find (playerSpellReference.spells [i].componentSpells [j].name);
+					tempCombo.localPosition = new Vector3 (600, highestCombo - spaceBetweenCombos * (spellNum - combosPerPage * currentPage), 0);
+					combos.Add (tempCombo);
 					spellNum++;
+
 				}
 			}
 		}
+		maxPage = currentPage;
+		SetPage (0);
 	}
 
+	void SetPage (int page) {
+		foreach (Transform combo in combos) {
+			combo.gameObject.SetActive (false);
+		}
+		for (int i = 0; i < combos.Count; i++) {
+			if (i >= combosPerPage * page && i < combosPerPage * (page + 1)) {
+				combos [i].gameObject.SetActive (true);
+			}
+			/*if (i > combosPerPage * page + 1) {
+				break;
+			}*/
+		}
+		currentPage = page;
+	}
 
+	public void NextPage () {
+		//print (currentPage);
+		if (currentPage != maxPage) {
+			SetPage (currentPage + 1);
+		}
+	}
+
+	public void PreviousPage () {
+		if (currentPage != 0) {
+			SetPage (currentPage - 1);
+		}
+	}
 }
