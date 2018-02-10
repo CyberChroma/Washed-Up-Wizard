f692d1afa1b2057a20be5c371b035c39da22a5f4
Brandon Howe <superspeedy1324@hotmail.com>
2018-02-10 01:03:06 -0500

    Updated more spells

--- Washed Up Wizard/Assets/Scripts/Move/RandomSpin.cs
+++ Washed Up Wizard/Assets/Scripts/Move/RandomSpin.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class RandomSpin : MonoBehaviour {
 
-    public float maxSpeed = 10; // The max rotational speed
+    public float maxSpeed = 10;
     public float smoothing = 1;
 
     void OnEnable () {
-        transform.rotation = Quaternion.Euler(maxSpeed * new Vector3(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed))); // Setting the rotational velocity
+        transform.rotation = Quaternion.Euler(new Vector3(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed))); // Setting the rotational velocity
     }
 
     // Update is called once per frame
     void FixedUpdate () {
-        transform.rotation = Quaternion.Euler(Vector3.Lerp(transform.rotation.eulerAngles, maxSpeed * new Vector3(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed)), smoothing * Time.deltaTime)); // Setting the rotational velocity
+        transform.Rotate (maxSpeed * new Vector3(Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed), Random.Range(-maxSpeed, maxSpeed)) * Time.deltaTime); // Spinning the object randomly
     }
 }
---
+++ Washed Up Wizard/Assets/Scripts/Move/SpinTransform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinTransform : MonoBehaviour {
+
+	public float rotSpeed = 10; // The rotational speed
+	public Vector3 dir = Vector3.zero; // The direction
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate (rotSpeed * dir * Time.deltaTime); // Rotating the object
+	}
+}
