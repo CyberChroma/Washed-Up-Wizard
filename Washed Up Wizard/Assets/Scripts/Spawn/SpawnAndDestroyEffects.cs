02b86583efcb0bed20ee8afc92d7f772846d74b8
TheLightLord <superspeedy1324@hotmail.com>
2018-02-16 11:03:58 -0500

    Created particle effects for when projectiles are spawned or destroyed

---
+++ Washed Up Wizard/Assets/Scripts/Spawn/SpawnAndDestroyEffects.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAndDestroyEffects : MonoBehaviour {
+
+    public GameObject spawnParticles;
+    public GameObject deathParticles;
+
+    void OnEnable () {
+        if (spawnParticles) {
+            Instantiate(spawnParticles, transform.position, Quaternion.identity);
+        }
+    }
+
+    void OnDisable () {
+        if (deathParticles) {
+            Instantiate(deathParticles, transform.position, Quaternion.identity);
+        }
+    }
+}
