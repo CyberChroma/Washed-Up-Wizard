05c7a92a6c69e2b845e035a155dc24e6a93c3120
TheLightLord <superspeedy1324@hotmail.com>
2018-03-07 14:37:11 -0500

    Created stomp attack

--- Washed Up Wizard/Assets/Scripts/EnemyAI/RingmasterAI.cs
+++ Washed Up Wizard/Assets/Scripts/EnemyAI/RingmasterAI.cs
@@ -7,14 +7,18 @@ public class RingmasterAI : MonoBehaviour {
     public enum AttackState {
         FlamingHoop,
         RollingBall,
-        HatBomb
+        HatBomb,
+        UnicycleCharge,
+        Stomp,
+        BalloonGun
     }
 
-    public float jumpForce;
+    public float airSpeed;
     public Transform player;
 
+    public float flamingHoopJumpForce;
     public float timeBetweenFlamingHoops;
-    public float timeBetweenJumps;
+    public float timeBetweenHoopJumps;
     public GameObject flamingHoopEmitter;
     public Transform[] jumpPoints;
 
@@ -30,6 +34,9 @@ public class RingmasterAI : MonoBehaviour {
     public GameObject acrobatEmitter;
     public float followSpeed;
 
+    public float timeBetweenStompJumps;
+    public Transform playerOldPos;
+
     public AttackState attackState;
     private bool canSpawn = false;
     private bool isJumping = false;
@@ -37,6 +44,7 @@ public class RingmasterAI : MonoBehaviour {
     private Transform movePos;
     private Rigidbody rb;
     private MoveByForce moveByForce;
+    private LaunchToTarget launchToTarget;
 
 	// Use this for initialization
 	void Awake () {
@@ -45,23 +53,32 @@ public class RingmasterAI : MonoBehaviour {
         rollingBallEmitter.SetActive(false);
         rb = GetComponent<Rigidbody>();
         moveByForce = GetComponent<MoveByForce>();
+        launchToTarget = GetComponent<LaunchToTarget>();
+        launchToTarget.enabled = false;
 	}
 	
     void OnEnable () {
         if (attackState == AttackState.FlamingHoop) {
             StartCoroutine(WaitToSpawn(timeBetweenFlamingHoops));
-            StartCoroutine(WaitToJump());
+            StartCoroutine(WaitToJump(timeBetweenHoopJumps));
+            moveByForce.force = airSpeed;
         }
         if (attackState == AttackState.RollingBall) {
-            rb.AddForce(Vector3.up * jumpForce * 100 * Time.deltaTime, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * flamingHoopJumpForce * 100 * Time.deltaTime, ForceMode.Impulse);
             movePos = pedistalJumpPoint;
             isJumping = true;
+            moveByForce.force = airSpeed;
         }
         if (attackState == AttackState.HatBomb)
         {
             StartCoroutine(WaitToSpawn(timeBetweenFlamingHoops));
             movePos = player;
             moveByForce.force = followSpeed;
+        }
+        if (attackState == AttackState.Stomp)
+        {
+            StartCoroutine(WaitToJump(timeBetweenStompJumps));
+            moveByForce.enabled = false;
         }
         StartCoroutine(WaitToSpawnAcrobats());
     }
@@ -100,8 +117,6 @@ public class RingmasterAI : MonoBehaviour {
         }
         else if (attackState == AttackState.HatBomb)
         {
-            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), 0.05f);
-            //transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.y, 0));
             if (canSpawn && !isJumping)
             {
                 bombEmitter.SetActive(true);
@@ -109,7 +124,11 @@ public class RingmasterAI : MonoBehaviour {
                 StartCoroutine(WaitToSpawn(timeBetweenHatBombs));
             }
             MoveToPos();
-
+        } else if (attackState == AttackState.Stomp) {
+            if (isJumping)
+            {
+                MoveToPos();
+            }
         }
         if (canSpawnAcrobats) {
             acrobatEmitter.transform.position = new Vector3 (Random.insideUnitCircle.x * 19, 0, Random.insideUnitCircle.y * 19);
@@ -126,7 +145,7 @@ public class RingmasterAI : MonoBehaviour {
 
     void MoveToPos () {
         moveByForce.dir = (movePos.position - transform.position);
-        if (attackState == AttackState.FlamingHoop && attackState == AttackState.RollingBall)
+        if (attackState == AttackState.FlamingHoop || attackState == AttackState.RollingBall)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, movePos.rotation, 0.05f);
         }
@@ -134,16 +153,25 @@ public class RingmasterAI : MonoBehaviour {
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), 0.1f);
         }
+        else if (attackState == AttackState.Stomp)
+        {
+            if (Vector3.Distance(new Vector3(movePos.position.x, 0, movePos.position.z), new Vector3(transform.position.x, 0, transform.position.z)) >= 1)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movePos.position - transform.position), 0.1f);
+            }
+        }
         transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
         if (Vector3.Distance(movePos.position, transform.position) <= 0.1f && rb.velocity.y <= 0) {
-            transform.rotation = movePos.rotation;
-            rb.velocity = Vector3.zero;
             moveByForce.dir = Vector3.zero;
             isJumping = false;
             canSpawn = true;
             if (attackState == AttackState.FlamingHoop)
             {
-                StartCoroutine(WaitToJump());
+                StartCoroutine(WaitToJump(timeBetweenHoopJumps));
+            }
+            else if (attackState == AttackState.Stomp)
+            {
+                StartCoroutine(WaitToJump(timeBetweenStompJumps));
             }
         } else if (Vector3.Distance(new Vector3 (movePos.position.x, 0, movePos.position.z), new Vector3(transform.position.x, 0, transform.position.z)) <= 0.1f) {
             moveByForce.dir = Vector3.zero;
@@ -152,16 +180,39 @@ public class RingmasterAI : MonoBehaviour {
         }
     }
 
-    IEnumerator WaitToJump () {
-        yield return new WaitForSeconds(timeBetweenJumps);
+    IEnumerator WaitToJump (float delay) {
+        yield return new WaitForSeconds(delay);
         canSpawn = false;
         if (attackState == AttackState.FlamingHoop)
         {
             movePos = jumpPoints[Random.Range(0, jumpPoints.Length - 1)];
+            rb.AddForce(Vector3.up * flamingHoopJumpForce * 100 * Time.deltaTime, ForceMode.Impulse);
         }
-        rb.AddForce(Vector3.up * jumpForce * 100 * Time.deltaTime, ForceMode.Impulse);
+        else if (attackState == AttackState.Stomp)
+        {
+            playerOldPos.position = new Vector3 (player.position.x, 0, player.position.z);
+            movePos = playerOldPos;
+            launchToTarget.enabled = true;
+        }
         yield return new WaitForSeconds(2 * Time.deltaTime);
         isJumping = true;
+    }
+
+    void OnCollisionEnter (Collision other) {
+        if (other.collider.CompareTag("Environment") && isJumping)
+        {
+            moveByForce.dir = Vector3.zero;
+            isJumping = false;
+            canSpawn = true;
+            if (attackState == AttackState.FlamingHoop)
+            {
+                StartCoroutine(WaitToJump(timeBetweenHoopJumps));
+            }
+            else if (attackState == AttackState.Stomp)
+            {
+                StartCoroutine(WaitToJump(timeBetweenStompJumps));
+            }
+        }
     }
 
     IEnumerator WaitToOpenCurtain (int curtainNum) {
