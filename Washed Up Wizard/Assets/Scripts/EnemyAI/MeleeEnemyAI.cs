d9434a9d32a210b0a5b3802ffa9fa3512388f322
nathan7777 <themonsterhunter777@gmail.com>
2018-03-07 13:35:18 -0400

    fixed collision bug

--- Washed Up Wizard/Assets/Scripts/EnemyAI/MeleeEnemyAI.cs
+++ Washed Up Wizard/Assets/Scripts/EnemyAI/MeleeEnemyAI.cs
@@ -71,6 +71,7 @@ public class MeleeEnemyAI : MonoBehaviour {
 	void OnCollisionEnter (Collision other) { // If the enemy collided with something, change its move target.
         if (other.collider.CompareTag ("Player") && anim) {
             anim.SetTrigger("Attack");
+            print("uhduuvndusnunvufsn");
         }
 	}
 }
