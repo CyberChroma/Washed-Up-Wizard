ef21f31542409f4dcb6c203c83057684d2590363
nathan7777 <themonsterhunter777@gmail.com>
2018-03-26 14:49:27 -0300

    dialogue system pretty much complete

---
+++ Washed Up Wizard/Assets/Pickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    [HideInInspector]
+    public bool taskComplete = false;
+
+    void OnTriggerEnter (Collider other){
+        taskComplete = true;
+    }
+}
--- Washed Up Wizard/Assets/Scripts/UI/Dialogue.cs
+++ Washed Up Wizard/Assets/Scripts/UI/Dialogue.cs
@@ -1,13 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
 public class Dialogue {
 
-    public string name;
-    public int numOfLines;
+    public enum TaskType
+    {
+        _pickup,
+        _killEnemies
+    }
 
-    [TextArea(3,100)]
-    public string[] sentences;
+    System.Random rand = new System.Random(); 
+
+    public Pickup pickup;
+    public killEnemies killEnemies;
+    public string name;//name of character
+    public TextAsset[] textReferences;//references to text files
+
+    public TaskType tasktype;
+    private int numOfLines;
+    private int whichSentence;
+
+    [HideInInspector]public string[] sentences;
+    [HideInInspector]public bool thankYou = false;
+
+    public void getSentences (){
+        numOfLines = textReferences.Length;
+        if (tasktype == TaskType._pickup)
+        {
+            if (pickup.taskComplete == false)
+            {
+                sentences = textReferences[numOfLines - 1].text.Split('\n');
+            }
+            else if (pickup.taskComplete == true && thankYou == false)
+            {
+                sentences = textReferences[numOfLines - 2].text.Split('\n');
+            }
+            else if (pickup.taskComplete == true && thankYou == true)
+            {
+                sentences = textReferences[rand.Next(0, numOfLines - 3)].text.Split('\n');
+            }
+        }
+        else if (tasktype == TaskType._killEnemies)
+        {
+            if (killEnemies.taskComplete == false)
+            {
+                sentences = textReferences[numOfLines - 1].text.Split('\n');
+            }
+            else if (killEnemies.taskComplete == true && thankYou == false)
+            {
+                sentences = textReferences[numOfLines - 2].text.Split('\n');
+            }
+            else if (killEnemies.taskComplete == true && thankYou == true)
+            {
+                sentences = textReferences[rand.Next(0, numOfLines - 3)].text.Split('\n');
+            }
+        }
+    }
 }
