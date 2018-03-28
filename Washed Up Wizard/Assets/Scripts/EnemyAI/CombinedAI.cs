74a7e84ad95fcbb96466f6e2c3e9e3da9683f519
TheLightLord <superspeedy1324@hotmail.com>
2018-03-28 13:18:37 -0400

    Brought back snowman
    Put snowman and slime monster in level

---
+++ Washed Up Wizard/Assets/Scripts/EnemyAI/CombinedAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinedAI : MonoBehaviour {
+
+    public float meleeRadius = 5;
+
+    private MeleeEnemyAI meleeEnemyAI;
+    private RangedEnemyAI rangedEnemyAI;
+    private MoveByConstantSpeed constantSpeed;
+    private Transform player;
+
+    void Awake () {
+        player = GameObject.Find("Player").transform;
+        rangedEnemyAI = GetComponent<RangedEnemyAI>();
+        meleeEnemyAI = GetComponent<MeleeEnemyAI>();
+        constantSpeed = GetComponent<MoveByConstantSpeed>();
+        rangedEnemyAI.enabled = false;
+        meleeEnemyAI.enabled = false;
+    }
+       
+
+    void FixedUpdate () {
+        if (player.position.x > transform.position.x - meleeRadius && player.position.x < transform.position.x + meleeRadius && player.position.z > transform.position.z - meleeRadius && player.position.z < transform.position.z + meleeRadius)
+        {
+            meleeEnemyAI.enabled = true;
+            constantSpeed.enabled = true;
+            rangedEnemyAI.enabled = false;
+        }
+        else{
+            meleeEnemyAI.enabled = false;
+            constantSpeed.enabled = false;
+            rangedEnemyAI.enabled = true;
+        }
+    }
+}
