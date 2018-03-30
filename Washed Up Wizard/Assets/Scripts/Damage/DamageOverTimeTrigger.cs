04453e54a92e9bf3fa11a94b907c5bfcc0b48679
TheLightLord <superspeedy1324@hotmail.com>
2018-03-29 22:17:54 -0400

    Set up spell types, added many potential sound tracks

--- Washed Up Wizard/Assets/Scripts/Damage/DamageOverTimeTrigger.cs
+++ Washed Up Wizard/Assets/Scripts/Damage/DamageOverTimeTrigger.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class DamageOverTimeTrigger : MonoBehaviour {
 
+    public Type type;
 	public float totalDamage = 5; // The total amount of damage to give
 	public float duration = 3; // The time over which to deal the damage
 	public bool oneTime = false; // Whether to only damage once
 	public string[] tagsToDamage; // The tags to damage
 
 	private bool damaged = false; // When this script has dealt damage (For oneTime)
+    private TakeDamage takeDamage;
 
 	void OnTriggerEnter (Collider other) {
-		if (oneTime && !damaged) { // If this script only deals damage once and it hasn't yet
-			foreach (string tag in tagsToDamage) { // Goes through each tag it can damage
-				if (other.CompareTag (tag)) { // If the collided object is something that can be damaged
-					other.GetComponent<TakeDamage> ().DamageOverTimeStart (totalDamage / duration, duration); // Sends the over time damage
-					damaged = true;
-				}
-			}
-		}
+        if (oneTime && !damaged) { // If this script only deals damage once and it hasn't yet
+            DamageOverTime (other);
+        }
 	}
 
 	void OnTriggerStay (Collider other) {
 		if (!oneTime) { // If this script doesn't only deals damage once
-			foreach (string tag in tagsToDamage) { // Goes through each tag it can damage
-				if (other.CompareTag (tag)) { // If the collided object is something that can be damaged
-					other.GetComponent<TakeDamage> ().DamageOverTimeStart (totalDamage / duration, duration); // Sends the over time damage
-				}
-			}
+            DamageOverTime (other);
 		}
 	}
-}
+
+    void DamageOverTime(Collider other)
+    {
+        foreach (string tag in tagsToDamage)
+        { // Goes through each tag it can damage
+            if (other.GetComponent<Collider>().CompareTag(tag))
+            { // If the collided object is something that can be damaged
+                if (other.GetComponent<TakeDamage>())
+                {
+                    takeDamage = other.GetComponent<TakeDamage>();
+                    if ((type == Type.Fire && takeDamage.type == Type.Fire) || (type == Type.Mechanical && takeDamage.type == Type.Mechanical) || (type == Type.Toxic && takeDamage.type == Type.Toxic) || (type == Type.Earth && takeDamage.type == Type.Earth) || (type == Type.Electrical && takeDamage.type == Type.Electrical) || (type == Type.Water && takeDamage.type == Type.Water))
+                    {
+                        takeDamage.Damage(0); // Sends the damage
+                    }
+                    else if ((type == Type.Fire && takeDamage.type == Type.Mechanical) || (type == Type.Mechanical && takeDamage.type == Type.Toxic) || (type == Type.Toxic && takeDamage.type == Type.Earth) || (type == Type.Earth && takeDamage.type == Type.Electrical) || (type == Type.Electrical && takeDamage.type == Type.Water) || (type == Type.Water && takeDamage.type == Type.Fire))
+                    {
+                        takeDamage.DamageOverTimeStart(totalDamage * 2 / duration, duration); // Sends the damage
+                    }
+                    else if ((type == Type.Fire && takeDamage.type == Type.Water) || (type == Type.Mechanical && takeDamage.type == Type.Fire) || (type == Type.Toxic && takeDamage.type == Type.Mechanical) || (type == Type.Earth && takeDamage.type == Type.Toxic) || (type == Type.Electrical && takeDamage.type == Type.Earth) || (type == Type.Water && takeDamage.type == Type.Electrical))
+                    {
+                        takeDamage.DamageOverTimeStart(totalDamage / 2 / duration, duration); // Sends the damage
+                    }
+                    else
+                    {
+                        takeDamage.DamageOverTimeStart(totalDamage / duration, duration); // Sends the damage
+                    }
+                    if (oneTime)
+                    {
+                        damaged = true; // Setting the bool
+                    }
+                }
+            }
+        }
+    }
+}           
