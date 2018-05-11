e4ef2f40dd2f1f1213b83add966a962304212071
nathan7777 <themonsterhunter777@gmail.com>
2018-05-11 15:57:30 -0300

    final fixes

---
+++ Washed Up Wizard/Assets/Scripts/UI/CursorChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorChange : MonoBehaviour {
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    private Vector2 hotSpot = Vector2.zero;
+    private GameObject PauseManager;
+    void Start()
+    {
+        PauseManager = GameObject.Find("Pause Manager");
+    }
+    void Update ()
+    {
+        if (!PauseManager.GetComponent<PauseManager>().isPaused)
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+        }
+        else if (PauseManager.GetComponent<PauseManager>().isPaused)
+        {
+            Cursor.SetCursor(null, hotSpot, cursorMode);
+        }
+    }
+}
