0531787303deaeaf3470b81154ed3925070ccb35
TheLightLord <superspeedy1324@hotmail.com>
2018-05-24 13:11:28 -0400

    Finished Hospital Intro cutscene

--- Washed Up Wizard/Assets/Scripts/Level Stuff/GameSaver.cs
+++ Washed Up Wizard/Assets/Scripts/Level Stuff/GameSaver.cs
@@ -101,7 +101,7 @@ public class GameSaver : MonoBehaviour {
                 levelButtons[i].SetActive(false);
             }
         }
-        else if (SceneManager.GetActiveScene().name == ("Slime Cube Boss") && unlockedLevel < 2)
+        else if (SceneManager.GetActiveScene().name == ("Slime Queen Boss") && unlockedLevel < 2)
         {
             unlockedLevel = 2;
         }
