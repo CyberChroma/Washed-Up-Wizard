6a3a21586fa187e81f0f055042c0f321e6896c0a
TheLightLord <superspeedy1324@hotmail.com>
2018-05-28 15:28:15 -0400

    More Improvements

---
+++ Washed Up Wizard/Assets/Scripts/Jiggler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jiggler : MonoBehaviour {
+
+    public float smoothing = 0.1f;
+
+    private Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(startPos.x + Random.Range(-1f, 1f), startPos.y + Random.Range(-1f, 1f), startPos.z + Random.Range(-1f, 1f)), smoothing * Time.deltaTime);
+    }
+}
