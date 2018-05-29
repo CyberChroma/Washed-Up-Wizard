ccc035c343c349c005ad2f111ce04f2e04f94057
nathan7777 <themonsterhunter777@gmail.com>
2018-05-29 10:15:58 -0400

    completed circus level 2, close to finishing 1 and 3

---
+++ Washed Up Wizard/Assets/Slow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Slow : MonoBehaviour {
+
+    public float slowSpeed = 3f;
+
+    private float hold;
+    private PlayerMoveInput player;
+
+	void Awake () {
+        player = FindObjectOfType<PlayerMoveInput>();
+	}
+	
+    void OnTriggerEnter (Collider other) {
+        if (other.CompareTag("Player"))
+        {
+        hold = player.moveSpeed;
+        player.moveSpeed = slowSpeed;
+        }
+	}
+
+    void OnTriggerExit(Collider other){
+        if(other.CompareTag("Player"))
+        {
+            player.moveSpeed = hold;
+        }
+    }
+}
